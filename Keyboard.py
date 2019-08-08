7b2bfe0e7907d87fc5856a2e7191664c13f6f162
randyt1027 <hotung1027@gmail.com>
2019-08-09 01:34:20 +0800

    init with curses library

--- Keyboard.py
+++ Keyboard.py
@@ -3,7 +3,7 @@ from queue import PriorityQueue
 
 class Button(Thread):
   #debug
-    def __init__(self, key, keylog, topics, CMD,OnCMD, OffCMD, lock, state = False, callback = None, buttonType = 'trigger'):
+    def __init__(self, key, topics,OnCMD, OffCMD, state = False, callback = None, buttonType = 'trigger'):
       # Invoke base class constructor before overriding  __init__ construct
       super().__init__()
 
@@ -12,12 +12,11 @@ class Button(Thread):
       # which button on keyboard to trigger the event
       ### TODO : Key Combination
       self.key = key
-      # which keylog to Listen
-      self.keylog = keylog
+      ### TODO : Share Keylogged character among Buttons
+      # # which keylog to Listen
+      # self.keylog = keylog
       # connect button event to selected topics
       self.topics = topics
-      # Which On/Off Command of the current state to send
-      self.CMD = CMD
       # On Toggle/Trigger Command
       self.OnCMD = OnCMD
       # Off Toggle Command
@@ -27,16 +26,16 @@ class Button(Thread):
       # Default state of the button event
       self.state = state
       # Threading Lock
-      self.lock = lock
+      self.lock = Semaphore(value=1)
       # Button type (Toggle/Trigger)
       self.buttonType = buttonType
        
-    def run(self):
-      
-      if ord(self.keylog) == ord(self.key):
-        self.lock.acquire()
-        self.callback()
-        self.lock.release()
+    # def run(self):
+    #   # ch = self.keylog()
+    #   if not len(ch) == 0 and ord(ch) == ord(self.key):
+    #     self.lock.acquire()
+    #     self.call()
+    #     self.lock.release()
 
 
     def args(self):
@@ -47,7 +46,7 @@ class Button(Thread):
       # default arugments
       def toggle(self):
 
-        self.lock.accquire()
+        self.lock.acquire()
 
         # inverse the current state at each time button is pressed and released
         self.state = not self.state
@@ -60,7 +59,7 @@ class Button(Thread):
 
       
       def trigger(self):
-        self.lock.accquire()
+        self.lock.acquire()
         # one time trigger upon pressed and released
         self.state = True
 
@@ -79,7 +78,7 @@ class Button(Thread):
       }
 
       # Trigger button event depend on button type on config
-      buttoneEvent.get(self.buttonType, lambda : None)()
+      buttoneEvent.get(self.buttonType, lambda : None)(self)
 
 
 
@@ -88,14 +87,14 @@ class Button(Thread):
 
       if self.state:
         #ifdebug
-        print("{current command:cmd}".format(cmd=str(self.OnCMD)))
+        print("current command:{cmd}".format(cmd=str(self.OnCMD)))
         #endif
         return self.OnCMD
         
 
       else:
         #ifdebug
-        print("{current command:cmd}".format(cmd=str(self.OnCMD)))
+        print("current command:{cmd}".format(cmd=str(self.OnCMD)))
         #endif
         return self.OffCMD
 
@@ -107,35 +106,28 @@ class Button(Thread):
 
 class KeyboardLogger(Thread):
   import curses
+  import sys
   def __init__(self):
     super().__init__(daemon=True)
     # return super().__init__(group, target, name, args, kwargs, *, daemon)
     
     self.keylog = PriorityQueue(maxsize=1)
     self.lock = Semaphore(2)
-    self.stdscr = self.terminal_init()
-    self.keystore = ''
+
+    # Initialize terminal  
+    self.stdscr = self.curses.initscr()
+    # self.keystore 
 
   def run(self):
-    while True:
-      if self.keylog.empty():
-        self.readKey()  
-        # self.update()
+    self.curses.wrapper(self.logkey())
 
-    
-  def terminal_init(self):
-    stdscr = self.curses.initscr()
-    self.curses.noecho()
-    self.curses.cbreak()
-    stdscr.keypad(True)
-    return stdscr
-
-  ### Closing the input
-  def terminal_close(self):
-    self.curses.nocbreak()
-    self.stdscr.keypad(False)
-    self.curses.echo()
-    self.curses.endwin()
+  def logkey(self):
+    self.curses.def_prog_mode()
+    try:
+      while True:
+        self.readKey()
+    except KeyboardInterrupt:
+      self.curses.endwin()
 
 
 
@@ -144,8 +136,6 @@ class KeyboardLogger(Thread):
     # accquire lock for reading
     self.lock.acquire()
     ch = self.stdscr.getkey()
-
-
     ### TODO : AVOID CONTINUOUS PRESSING CMD - REQUIRE PRESS AND RELEASE FOR EACH ACTIVITY
     self.keylog.put(ch, block=True)
     # release lock
@@ -154,5 +144,22 @@ class KeyboardLogger(Thread):
   def get(self):
     return self.keylog.get()
 
-  def udpate(self):
-    self.keystore = self.get()
+  def __enter__(self):
+    self.curses.savetty()
+    self.curses.def_shell_mode()
+    self.curses.noecho()
+    self.curses.cbreak()
+    self.curses.nl()
+    self.stdscr.keypad(True)
+    return self
+
+
+  def __exit__(self,exec_type,exc_value,traceback):
+    self.curses.resetty()
+    self.curses.endwin()
+
+  # def update(self):
+  #   self.keystore = self.get()
+
+  # def read(self):
+  #   return self.keystore
