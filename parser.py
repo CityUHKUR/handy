4e6ba22a231c8315c6a92d1bc7c0e072d5569d25
randyt1027 <hotung1027@gmail.com>
2019-08-24 21:07:39 +0800

    created parser.py with predefined methods

---
+++ parser.py
@@ -0,0 +1,24 @@
+class Parser:
+    import csv
+    import numpy as np
+    """
+    a class parsing file into rows of parameters and initialize functions
+    """
+    def __init__(self, filename, dtypes, delimiter = ' ', *args, **kwargs):
+
+    
+    def __read__(filename, delimiter = ' '):
+
+    
+    def initialize(self, init_class, callback, pos_index = self._idx(1,5)):
+        """
+        initialize objects for key mapping, return all initialized obj afterward
+
+        Assume key \'Key\' must be exist
+        """
+
+    
+    def _idx(__from__ = 1, __to__ = 5):
+
+
+
